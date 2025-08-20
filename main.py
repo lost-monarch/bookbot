ecb6c45c0c8a674f17b1426ee7e48ef162fba6ab
lost-monarch <oac.bwe@gmail.com>
2025-08-19 21:39:19 -0400

    added sys.argv functionality

--- main.py
+++ main.py
@@ -1,25 +1,30 @@
 from stats import word_count
 from stats import character_count
 from stats import char_sort
+import sys
+
+if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
 def main():
-    character_dict = character_count(get_book_text("books/frankenstein.txt"))
+    character_dict = character_count(get_book_text(sys.argv[1]))
 
-    words = word_count(get_book_text("book/frankenstein.txt"))
+    words = word_count(get_book_text(sys.argv[1]))
 
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {sys.argv[1]}...")
     print("----------- Word Count ----------")
-    print(f"found {words} total words")
+    print(f"Found {words} total words")
     print("--------- Character Count -------")
 
     for i in char_sort(character_dict):
         if i["char"].isalpha():
-            print(f"{i['char']} : {i['num']}")
+            print(f"{i['char']}: {i['num']}")
 
     print("============= END ===============")
 
