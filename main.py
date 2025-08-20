9aee3a42b3fb8127b09f75a94d5c0f410f2e0863
lost-monarch <oac.bwe@gmail.com>
2025-08-19 21:42:18 -0400

    added variable path_to_book

--- main.py
+++ main.py
@@ -7,17 +7,19 @@ if len(sys.argv) < 2:
     print("Usage: python3 main.py <path_to_book>")
     sys.exit(1)
 
+path_to_book = sys.argv[1]
+
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
 def main():
-    character_dict = character_count(get_book_text(sys.argv[1]))
+    character_dict = character_count(get_book_text(path_to_book))
 
-    words = word_count(get_book_text(sys.argv[1]))
+    words = word_count(get_book_text(path_to_book))
 
     print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {sys.argv[1]}...")
+    print(f"Analyzing book found at {path_to_book}...")
     print("----------- Word Count ----------")
     print(f"Found {words} total words")
     print("--------- Character Count -------")
